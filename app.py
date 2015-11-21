5946475afe41e30ea5cfc07b9c82965a2c001a49
Chester Holtz <chesterholtz@gmail.com>
2015-11-21 18:27:34 -0500

    bad aborts and started groups - 6:27 pm

--- app.py
+++ app.py
@@ -8,13 +8,20 @@ app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
 # -- will move models to seperate file --
+# relationship table
+group_table = db.Table('groups',
+    db.Column('group_id', db.Integer, db.ForeignKey('group.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 # user model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.String(20))
     first_name = db.Column(db.String(20))
     last_name = db.Column(db.String(20))
-    groups = db.Column(db.String(50)) # assume groups are single words without spaces
+    #groups = db.Column(db.String(50)) # assume groups are single words without spaces
+    #group = db.relationship('Group',backref='user',lazy='dynamic')
 
     def __init__(self, userid, first_name, last_name, groups):
         self.userid = userid
@@ -26,6 +33,7 @@ class User(db.Model):
     def as_dict(self):
         return{c.name: getattr(self,c.name)for c in self.__table__.columns}
 
+#group model
 class Group(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     group_name = db.Column(db.String(20))
@@ -33,11 +41,19 @@ class Group(db.Model):
     def __init__(self, group_name):
         self.group_name = group_name
 
-db.create_all()
-# -------- USERS 
+    def as_dict(self):
+        return{c.name: getattr(self,c.name)for c in self.__table__.columns}
 
+db.create_all()
+
+
+# -------- USERS 
+'''
+GET /users/<userid>
+    Returns the matching user record or 404 if none exist.
+'''
 @app.route('/users/', methods = ['GET'])
-def index():
+def get_users():
     user = User.query.all()
     users = []
     if(user is not None):
@@ -47,25 +63,41 @@ def index():
     else:
         return jsonify({'users':'none'})
 
+@app.route('/users/<userid>', methods = ['GET'])
+def get_user(userid):
+    user = User.query.filter_by(userid=userid).first()
+    if (user is None):
+        abort(404)
+    return jsonify({'user': user.as_dict()})
+
+'''
+POST /users
+    Creates a new user record. The body of the request should be a valid user
+    record. POSTs to an existing user should be treated as errors and flagged
+    with the appropriate HTTP status code.
+'''
 @app.route('/users/', methods = ['POST'])
 def create_user():
     # userid should be mandatory for user creation
     if not request.json or not 'userid' in request.json:
         abort(400)
+    if User.query.filter_by(userid=request.json['userid']).first() is not None:
+        abort(404) # user already exists
     user = User(request.json['userid'],request.json.get('first_name',''),request.json.get('last_name',''), request.json.get('groups',''))
     db.session.add(user)
     db.session.commit()
     return jsonify({'user':user.as_dict()})
 
-@app.route('/users/<userid>', methods = ['DELETE'])
-def delete_user(userid):
-    db.session.delete(User.query.filter_by(userid = userid).first()) # userid should be unique
-    db.session.commit()
-    return jsonify({'result': True})
-
+'''
+PUT /users/<userid>
+    Updates an existing user record. The body of the request should be a valid
+    user record. PUTs to a non-existant user should return a 404.
+'''
 @app.route('/users/<userid>', methods = ['PUT'])
 def update_user(userid):
     user = User.query.filter_by(userid = userid).first()
+    if (user is None):
+        abort(404) # user does not exist
     user.userid = request.json.get('userid', user.userid)
     user.first_name = request.json.get('first_name', user.first_name)
     user.last_name = request.json.get('last_name', user.last_name)
@@ -73,7 +105,69 @@ def update_user(userid):
     db.session.commit()
     return jsonify({'user':user.as_dict()})
 
-# -------- GROUPS (ignore for now)
+'''
+DELETE /users/<userid>
+    Deletes a user record. Returns 404 if the user doesn't exist.
+'''
+@app.route('/users/<userid>', methods = ['DELETE'])
+def delete_user(userid):
+    user = User.query.filter_by(userid = userid).first()
+    if (user is None):
+        abort(404) # user does not exist
+    db.session.delete(user) # userid should be unique
+    db.session.commit()
+    return jsonify({'result': True})
+
+
+# -------- GROUPS
+'''
+GET /groups/<group name>
+    Returns a JSON list of userids containing the members of that group. Should
+    return a 404 if the group doesn't exist.
+'''
+@app.route('/groups/', methods = ['GET'])
+def get_groups():
+    return 0
+
+'''
+POST /groups
+    Creates a empty group. POSTs to an existing group should be treated as
+    errors and flagged with the appropriate HTTP status code. The body should contain
+    a `name` parameter
+'''
+@app.route('/groups/', methods = ['POST'])
+def create_group():
+    if not request.json or not 'group_name' in request.json:
+        abort(400)
+    if Group.query.filter_by(group_name=request.json['group_name']).first() is not None:
+        abort(404) # user already exists
+    group = Group(request.json['group_name'])
+    db.session.add(group)
+    db.session.commit()
+    return jsonify({'group':group.as_dict()})
+
+'''
+PUT /groups/<group name>
+    Updates the membership list for the group. The body of the request should 
+    be a JSON list describing the group's members.
+'''
+@app.route('/groups/<group_name>', methods = ['PUT'])
+def update_group(group_name):
+   return 0
+
+'''
+DELETE /groups/<group name>
+    Deletes a group.
+'''
+@app.route('/groups/<group_name>', methods = ['DELETE'])
+def delete_group(group_name):
+    group = Group.query.filter_by(group_name = group_name).first()
+    if (group is None):
+        abort(404) # user does not exist
+    db.session.delete(group) # group_name should be unique
+    db.session.commit()
+    return jsonify({'result': True})
+
 
 if __name__ == '__main__':
     app.debug = True
