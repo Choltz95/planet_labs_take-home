d5e0dad8495f5304f9729a61d308b585b0a5ef20
Chester Holtz <chesterholtz@gmail.com>
2015-11-22 15:29:27 -0500

    typo

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLAlCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 SQLALCHEMY_MIGRATEREPO = os.path.join(basedir,'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
